49bea68449dda74f3c30cf875b3be78e913b7593
Roosembert Palacios <roosemberth.palacios@epfl.ch>
2016-03-20 01:42:07 +0100

    Update for new Computer :D
    Signed-off-by: Roosembert Palacios <roosemberth.palacios@epfl.ch>

--- GPScripts/battery_indicator.sh
+++ GPScripts/battery_indicator.sh
@@ -6,19 +6,33 @@ PLUG='🔌'
 
 ICON=${BATTERY}
 
-if [ -z "$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/status' | grep Discharging)" ]; then
+# -- Uncomment a single line
+# HP Pavillon DV4-4062la 
+# CHARGING="$([ -z "$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/status' | grep Discharging)" ] && echo true)"
+# Lenovo Thinkpad P50
+CHARGING=$([ $(cat /sys/class/power_supply/AC/online) -gt 0 ] && echo true)
+# --
+
+if [ "$CHARGING" = "true" ]; then
 	ICON=${PLUG}
 fi
 
+# -- Battery status: Uncomment a single block
+# HP Pavillon DV4-4062la
+#CURRENT_CHARGE=$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/CHARGE_now')
+#TOTAL_CHARGE=$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/CHARGE_full')
+# - OSx
+#battery_info=`ioreg -rc AppleSmartBattery`
+#CURRENT_CHARGE=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+#TOTAL_CHARGE=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+# Lenovo Thinkpad P50
+CURRENT_CHARGE=$(cat /sys/class/power_supply/BAT0/energy_now)
+TOTAL_CHARGE=$(cat /sys/class/power_supply/BAT0/energy_full)
 
-if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/charge_now')
-  total_charge=$(cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/charge_full')
+if [ -x /usr/bin/bc ]; then
+	CHARGE=$(echo "scale=4;((100*$CURRENT_CHARGE)/$TOTAL_CHARGE)" | bc -l)
 else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+	CHARGE=$((100*$CURRENT_CHARGE/$TOTAL_CHARGE))
 fi
 
-charge=$(echo "((100*$current_charge)/$total_charge)" | bc -l | cut -d '.' -f 1)
-echo "${ICON} ${charge}%"
+echo "${ICON} ${CHARGE}%"
