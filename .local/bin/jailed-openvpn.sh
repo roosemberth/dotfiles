1b68a07c3ee466969eda314050236193a3d26df2
Roosembert Palacios <roosembert.palacios@epfl.ch>
2017-10-05 20:53:26 +0200

    Create network jail with ovpn route
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

---
+++ .local/bin/jailed-openvpn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Usage: JAIL="ovpn-tb-usa"; sudo openvpn --config "TunnelBear United States.ovpn" --auth-user-pass =(pass show web/tunnelbear.com/1) --route-noexec --ifconfig-noexec --route-up "$(which jailed-openvpn.sh) ${JAIL}" --up "$(which jailed-openvpn.sh) ${JAIL}" --script-security 2
+
+JAIL_NAME=${1:-OpenVPN--tmp}
+
+case $script_type in
+    up)
+        ip netns add $JAIL_NAME
+        ip netns exec $JAIL_NAME ip link set dev lo up
+        ip netns exec $JAIL_NAME ip addr add 127.0.0.1/8 dev lo
+        ip link set dev "$dev" up netns $JAIL_NAME mtu "$link_mtu"
+        ip netns exec $JAIL_NAME ip addr add dev "$dev" "${ifconfig_local}"/"${ifconfig_netmask:-30}"
+        ip netns exec $JAIL_NAME ip -6 addr add dev "$dev" "${ifconfig_ipv6_local}"/"${ifconfig_ipv6_netbits:-64}" 
+        ;;
+    route-up)
+        ip netns exec $JAIL_NAME ip route add default via "$route_vpn_gateway"
+        ;;
+    down)
+        ip netns delete $JAIL_NAME
+        ;;
+esac
