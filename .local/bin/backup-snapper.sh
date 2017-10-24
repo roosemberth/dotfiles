efd6232c8f378d2a01edd644856df3fa5deb23f8
Roosembert Palacios <roosembert.palacios@epfl.ch>
2017-10-24 18:30:17 +0200

    backup-snapper.sh: Made it POSIX compilant, small changes. Thanks to Sparrowhawk!
    vv Original edit message
    
    Edited to suit my needs by Sparrowhawk, in a CS206 class, anno domini 2017.
    Made it POSIX sh compliant, though rather arch-centric (will later be corrected).
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- .local/bin/backup-snapper.sh
+++ .local/bin/backup-snapper.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 # Roosembert Palacios (2017)
+
+# shellcheck source=/dev/null
+# shellcheck disable=2059
+# shellcheck disable=2029
 
 # Exit codes
 E_SUCCESS=0
@@ -44,9 +48,9 @@ usage()
 	user_data as parent subvolume when invoking btrfs {send,receive}.
 
 	Destination subvolumes will be renamed to the following pattern:
-	\$(date +%y%m%d-%H%M)-\$(echo \${description} | sed 's/[^A-Za-z0-9]/_/g')
+	\$(date +%F-%H%M)-\$(echo \${description} | sed 's/[^A-Za-z0-9]/_/g')
 	where description corresponds to the snapper description replacing everything
-	but A-Z a-z 0-9 by '_'. ie. '171006-0130-I_d_cross_the_universe_for_you'
+	but A-Z a-z 0-9 by '_'. ie. '2017-10-06-0130-I_d_cross_the_universe_for_you'
 
 	$BASENAME will ssh multiple times to the remote host. use of an ssh agent is recommended.
 
@@ -76,8 +80,8 @@ usage()
 	            snapshot description is "Hello World".
 
 	            This will create read-only subvolumes
-	            - /mnt/Freezer/var/stores/Triglav/root/$(date +%y%m%d-%H%M)-Hello_World
-	            - /mnt/Freezer/var/stores/Triglav/homes/$(date +%y%m%d-%H%M)-Hello_World
+	            - /mnt/Freezer/var/stores/Triglav/root/$(date +%F-%H%M)-Hello_World
+	            - /mnt/Freezer/var/stores/Triglav/homes/$(date +%F-%H%M)-Hello_World
 
 	    $BASENAME --remote root@10.13.13.1 --dest /mnt/Freezer/var/stores/Triglav
 	            Will send the btrfs snapshots via ssh. ie
@@ -100,21 +104,24 @@ done
 
 set -e
 
-if ! which pv; then
-    die $E_NOCMD "pv must be accessible via PATH"
-fi
+for prog in pv grep awk;
+do
+    if ! which "$prog"; then
+        die $E_NOCMD "$prog must be accessible via PATH"
+    fi
+done;
 
 if [ "$(id -u)" != '0' ]; then
     die $E_USER "Script must be run as root."
 fi
 
-if [ -z $BACKUPDIR ]; then
+if [ -z "$BACKUPDIR" ]; then
     die $E_USER "ERROR: External backup location not set!"
 fi
 
 if [ -z "$REMOTE_SSH" ]; then
-    if [ ! -d $BACKUPDIR ]; then
-        die $E_USER "ERROR: $BACKUPDIR is not a directory."
+    if [ ! -d "$BACKUPDIR" ]; then
+        die "$E_USER" "ERROR: $BACKUPDIR is not a directory."
     fi
 else
     if ssh "$REMOTE_SSH" '[ ! -d $BACKUPDIR ]' ; then
@@ -125,15 +132,15 @@ fi
 echo "Processing subvolumes $(echo $CONFIGS | tr '\n' ' ')"
 
 for x in $CONFIGS; do
-    source /etc/snapper/configs/$x
+    . /etc/snapper/configs/$x
 
     DO_BACKUP=${EXT_BACKUP:-"yes"}
 
-    if [[ $DO_BACKUP == "yes" ]]; then
-        OLD_NUMBER=$(snapper -c $x list -t single | grep "$OLD_USERDATA"| awk '/'"$OLD_USERDATA"'/ {print $1}' | head -n 1)
-        NEW_NUMBER=$(snapper -c $x list -t single | grep "$NEW_USERDATA"| awk '/'"$NEW_USERDATA"'/ {print $1}' | head -n 1)
+    if [ "$DO_BACKUP" = "yes" ]; then
+        OLD_NUMBER=$(snapper -c "$x" list -t single | grep "$OLD_USERDATA"| awk '/'"$OLD_USERDATA"'/ {print $1}' | head -n 1)
+        NEW_NUMBER=$(snapper -c "$x" list -t single | grep "$NEW_USERDATA"| awk '/'"$NEW_USERDATA"'/ {print $1}' | head -n 1)
 
-        if [[ -z "$NEW_NUMBER" ]]; then
+        if [ -z "$NEW_NUMBER" ]; then
             echo "Target snapshot for configuration $x not found, add extbackup=please as userdata to tag target snapshot. Skipping"
             continue
         fi
@@ -145,12 +152,12 @@ for x in $CONFIGS; do
             ssh "$REMOTE_SSH" mkdir -p "$BACKUP_LOCATION"
         fi
 
-        NEW_ROW=$(snapper -c $x list -t single | egrep "^$NEW_NUMBER +\|")
-        NEW_DATE=$(date --date="$(echo $NEW_ROW | awk -F '|' '{print $2}')" "+%y%m%d-%H%M")
+        NEW_ROW=$(snapper -c "$x" list -t single | grep -E "^$NEW_NUMBER +\|")
+        NEW_DATE=$(date --date="$(echo "$NEW_ROW" | awk -F '|' '{print $2}')" "+%F-%H%M")
         NEW_ORIG_SNAPSHOT="$SUBVOLUME/.snapshots/$NEW_NUMBER/snapshot"
         NEW_ORIG_INFO="$SUBVOLUME/.snapshots/$NEW_NUMBER/info.xml"
-        NEW_DESCRIPTION="$(echo $NEW_ROW | awk -F '|' '{print $4}' | sed 's/^ *//;s/ *$//')"
-        NEW_SNAPSHOT_NAME="${NEW_DATE}-$(echo $NEW_DESCRIPTION | sed 's/[^A-Za-z0-9]/_/g')"
+        NEW_DESCRIPTION="$(echo "$NEW_ROW" | awk -F '|' '{print $4}' | sed 's/^ *//;s/ *$//')"
+        NEW_SNAPSHOT_NAME="${NEW_DATE}-$(echo "$NEW_DESCRIPTION" | sed 's/[^A-Za-z0-9]/_/g')"
         NEW_SNAPSHOT="$SUBVOLUME/.snapshots/$NEW_NUMBER/$NEW_SNAPSHOT_NAME"
 
         mv "$NEW_ORIG_SNAPSHOT" "$NEW_SNAPSHOT"
@@ -159,21 +166,19 @@ for x in $CONFIGS; do
             echo "Will perform initial backup for snapper configuration '$x'."
 
             if [ -z "$REMOTE_SSH" ]; then
-                btrfs send $NEW_SNAPSHOT | pv -pbtea | btrfs receive $BACKUP_LOCATION
+                btrfs send "$NEW_SNAPSHOT" | pv -pbtea | btrfs receive "$BACKUP_LOCATION"
             else
-                btrfs send $NEW_SNAPSHOT | pv -pbtea | ssh "$REMOTE_SSH" btrfs receive $BACKUP_LOCATION
+                btrfs send "$NEW_SNAPSHOT" | pv -pbtea | ssh "$REMOTE_SSH" btrfs receive "$BACKUP_LOCATION"
             fi
 
             sudo mv "$NEW_SNAPSHOT" "$NEW_ORIG_SNAPSHOT"
         else
-            OLD_ROW=$(snapper -c $x list -t single | egrep "^$OLD_NUMBER +\|")
-            OLD_DATE=$(date --date="$(echo $OLD_ROW | awk -F '|' '{print $2}')" "+%y%m%d-%H%M")
             OLD_ORIG_SNAPSHOT="$SUBVOLUME/.snapshots/$OLD_NUMBER/snapshot"
 
             if [ -z "$REMOTE_SSH" ]; then
-                btrfs send -p $OLD_ORIG_SNAPSHOT $NEW_SNAPSHOT | pv -pbtea | btrfs receive -v $BACKUP_LOCATION
+                btrfs send -p "$OLD_ORIG_SNAPSHOT" "$NEW_SNAPSHOT" | pv -pbtea | btrfs receive -v "$BACKUP_LOCATION"
             else
-                btrfs send -p $OLD_ORIG_SNAPSHOT $NEW_SNAPSHOT | pv -pbtea | ssh "$REMOTE_SSH" btrfs receive -v $BACKUP_LOCATION
+                btrfs send -p "$OLD_ORIG_SNAPSHOT"  "$NEW_SNAPSHOT" | pv -pbtea | ssh "$REMOTE_SSH" btrfs receive -v "$BACKUP_LOCATION"
             fi
 
             sudo mv "$NEW_SNAPSHOT" "$NEW_ORIG_SNAPSHOT"
@@ -181,12 +186,14 @@ for x in $CONFIGS; do
             if [ -z "$REMOTE_SSH" ]; then
                 cp "$NEW_ORIG_INFO" "$BACKUP_LOCATION/$NEW_SNAPSHOT_NAME.xml"
             else
-                cat "$NEW_ORIG_INFO" | ssh "$REMOTE_SSH" tee "$BACKUP_LOCATION/$NEW_SNAPSHOT_NAME.xml" > /dev/null
+                ssh "$REMOTE_SSH" tee "$BACKUP_LOCATION/$NEW_SNAPSHOT_NAME.xml" < "$NEW_ORIG_INFO" > /dev/null
             fi
-            snapper -c $x delete $OLD_NUMBER
+            snapper -c "$x" delete "$OLD_NUMBER"
         fi
 
         # Tag new snapshot as the latest
-        snapper -v -c $x modify -d "$NEW_DESCRIPTION" -u "$OLD_USERDATA" -c "" $NEW_NUMBER
+        snapper -v -c "$x" modify -d "$NEW_DESCRIPTION" -u "$OLD_USERDATA" -c "" "$NEW_NUMBER"
     fi
 done
+
+return $E_SUCCESS
