3b35cecb77467597252c370e78dbead0e527b051
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-24 00:38:30 +0100

    + nixos-config: New derivation for 35C3!
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

---
+++ nixos-config/machines/Triglav-ccc.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [ ./Triglav.nix ];
+  networking.hostName = lib.mkForce "Triglav-ccc";
+  networking.extraHosts = lib.mkForce ''
+    127.0.0.1 Triglav-ccc triglav-ccc.roaming.orbstheorem.ch
+    5.2.74.181 Hellendaal hellendaal.orbstheorem.ch
+    46.101.112.218 Heisenberg heisenberg.orbstheorem.ch
+    95.183.51.23 Dellingr dellingr.orbstheorem.ch
+  '';
+  users.extraUsers.roosemberth.hashedPassword =
+    lib.mkForce "$6$FRRhyJnGdPTm5UM2$cPFoqHq9Av.EFIrJ1c5Poj7quICfGeu7iWTkXhJcsNBed94Gl5ZKgo8YRFcLcd3FWm2pEuspBSHvi6nCxTCm60";
+}
--- nixos-config/machines/machines.nix
+++ nixos-config/machines/machines.nix
@@ -4,4 +4,8 @@
     bootPartUuid = "6283960e-0d80-4455-a72a-3318c34cd1fb";
     rootDeviceUuid = "cd56ef5b-74bd-426e-96de-c1ccd2b0de72";
   };
+  Triglav-ccc = {
+    bootPartUuid = "c8b1bb13-bece-4653-a96e-c89cae66805c";
+    rootDeviceUuid = "ccb70642-99ee-4509-8dde-3c831abf395e";
+  };
 }
