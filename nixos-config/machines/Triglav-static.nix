338e64ae43d86a597d02e6e887166f961fc38b36
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-23 23:48:57 +0100

    nixos-config: Triglav: Extract machine physical configuration from static
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- nixos-config/machines/Triglav-static.nix
+++ nixos-config/machines/Triglav-static.nix
@@ -2,12 +2,7 @@
 
 let
   hostname = config.networking.hostName;
-  uuid = {
-    triglav = "cd56ef5b-74bd-426e-96de-c1ccd2b0de72";
-  };
-  partuuid = {
-    boot = "6283960e-0d80-4455-a72a-3318c34cd1fb";
-  };
+  machine = builtins.getAttr hostname (import ./machines.nix {});
   secrets = import ../secrets.nix { inherit lib; };
 in
 {
@@ -21,8 +16,8 @@ in
       luks = {
         devices = [
           {
-            name = "Triglav";
-            device = "/dev/disk/by-uuid/${uuid.triglav}";
+            name = hostname;
+            device = "/dev/disk/by-uuid/${machine.rootDeviceUuid}";
           }
         ];
       };
@@ -52,66 +47,66 @@ in
     "/" = {
       fsType = "btrfs";
       mountPoint = "/";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/.__active__/rootfs" "defaults" "noatime" "compress=zlib" "autodefrag"];
     };
     "/.snapshots" = {
       fsType = "btrfs";
       mountPoint = "/.snapshots";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/snapshots/rootfs" "defaults" "noatime"];
     };
     "/boot" = {
       fsType = "vfat";
       mountPoint = "/boot";
-      device = "/dev/disk/by-partuuid/${partuuid.boot}";
+      device = "/dev/disk/by-partuuid/${machine.bootPartUuid}";
     };
     "/var" = {
       fsType = "btrfs";
       mountPoint = "/var";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/.__active__/var" "defaults" "noatime" "compress=zlib" "autodefrag"];
     };
     "/home" = {
       fsType = "btrfs";
       mountPoint = "/home";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/.__active__/homes" "defaults" "noatime" "compress=zlib" "autodefrag"];
     };
     "/Storage" = {
       fsType = "btrfs";
       mountPoint = "/Storage";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/.__active__/Storage" "defaults" "noatime" "compress=zlib" "autodefrag"];
     };
     "/Storage/DevelHub" = {
       fsType = "btrfs";
       mountPoint = "/Storage/DevelHub";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/.__active__/DevelHub" "defaults" "noatime" "compress=zlib" "autodefrag"];
     };
     "/home/.snapshots" = {
       fsType = "btrfs";
       mountPoint = "/home/.snapshots";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/snapshots/homes" "defaults" "noatime"];
     };
     "/Storage/.snapshots" = {
       fsType = "btrfs";
       mountPoint = "/Storage/.snapshots";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/snapshots/Storage" "defaults" "noatime"];
     };
     "/Storage/DevelHub/.snapshots" = {
       fsType = "btrfs";
       mountPoint = "/Storage/DevelHub/.snapshots";
-      device = "/dev/mapper/Triglav";
+      device = "/dev/mapper/" + hostname;
       options = ["subvol=/subvolumes/snapshots/DevelHub" "defaults" "noatime"];
     };
-    "/mnt/root-btrfs-Triglav" = {
+    "/mnt/root-btrfs" = {
       fsType = "btrfs";
-      mountPoint = "/mnt/root-btrfs-Triglav";
-      device = "/dev/mapper/Triglav";
+      mountPoint = "/mnt/root-btrfs";
+      device = "/dev/mapper/" + hostname;
       options = ["nodatacow" "noatime" "noexec"];
     };
   };
---
+++ nixos-config/machines/machines.nix
@@ -0,0 +1,7 @@
+{}:
+{
+  Triglav = {
+    bootPartUuid = "6283960e-0d80-4455-a72a-3318c34cd1fb";
+    rootDeviceUuid = "cd56ef5b-74bd-426e-96de-c1ccd2b0de72";
+  };
+}
