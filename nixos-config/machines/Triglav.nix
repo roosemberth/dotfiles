9d4dd2bf1fa2caeec92d30ce5d7a6f7dc9eeb03e
Roosembert Palacios <roosembert.palacios@epfl.ch>
2019-02-18 22:49:24 +0100

    Triglav.nix: Sync packages + use nvidia proprietary driver :(
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- nixos-config/machines/Triglav.nix
+++ nixos-config/machines/Triglav.nix
@@ -29,6 +29,10 @@ in
     pulseaudio.enable = true;
     pulseaudio.package = pkgs.pulseaudioFull;
     pulseaudio.support32Bit = true;     # Steam...
+
+    bumblebee.enable = true;
+    bumblebee.connectDisplay = true;
+    nvidia.modesetting.enable = true;
   };
 
   i18n.consoleFont = "sun12x22";
@@ -77,9 +81,9 @@ in
               };
           }));
         });
-        mymopidy = with pkgs; buildEnv {
+        mymopidy = with bleedingEdge; buildEnv {
           name = "mopidy-with-extensions";
-          paths = lib.closePropagation (with pkgs; [mopidy-spotify mopidy-iris]);
+          paths = lib.closePropagation (with bleedingEdge; [mopidy-spotify mopidy-iris]);
           pathsToLink = [ "/${python.sitePackages}" ];
           buildInputs = [ makeWrapper ];
           postBuild = ''
@@ -113,6 +117,10 @@ in
   };
 
   services = {
+    kubernetes = {
+      roles = ["master" "node"];
+    };
+
     logind.lidSwitch = "ignore";
     logind.extraConfig = ''
       HandlePowerKey="ignore"
@@ -213,64 +221,64 @@ in
     extraGroups = ["docker" "libvirtd" "networkmanager" "wheel" "wireshark"];
     packages = (with pkgs; [ # Legacy
       astyle baobab bc beets bind blender bluez coreutils cpufrequtils
-      darktable dfu-util dico dmidecode dnsutils docker dolphin doxygen dunst
+      darktable dfu-util dmidecode dnsutils docker dolphin doxygen dunst
       enlightenment.terminology evtest exfat exif fbida fbterm feh ffmpeg file
-      firefox firejail fontforge fortune geteltorito gftp ghostscript gimp
+      firefox firejail fontforge geteltorito gftp ghostscript gimp
       gitAndTools.git-annex gitAndTools.git-crypt glxinfo gnupg go-mtpfs
-      gparted graphviz gucharmap hack-font i3 i7z imagemagick imv intel-gpu-tools
-      iw jq khal kicad # libnfs
+      gparted graphviz gucharmap hack-font i7z imagemagick imv intel-gpu-tools
+      iw jq khal # libnfs
       libnotify libreoffice libtool libvirt libxml2 lm_sensors lshw lxappearance
       man-pages megatools minicom mkpasswd moreutils mosh mpc_cli mpd mr
       mtpfs mypy ncftp ncmpc ncmpcpp neomutt neovim nethogs nfs-utils nitrogen
       nmap nss numix-solarized-gtk-theme oathToolkit offlineimap openconnect
       openjdk openssh openssl openvpn pamix pandoc pass patchelf pavucontrol
       pbzip2 pciutils pdftk picocom pipenv postgresql powerline-fonts powertop
-      ppp pptp profont proxychains psmisc pv python3 qutebrowser radare2 ranger
+      ppp pptp profont proxychains psmisc pv qutebrowser radare2 ranger
       read-edid redshift remmina rfkill rtorrent rxvt_unicode-with-plugins s3cmd
-      sakura screen-message scrot shutter silver-searcher smartmontools socat
+      sakura scrot shutter smartmontools
       source-code-pro splint sshfs sshfs-fuse ssvnc stack stdman stress sway
-      swig sysstat tasknc taskwarrior tcpdump terminus_font_ttf
+      swig sysstat tasknc taskwarrior terminus_font_ttf
       texstudio timewarrior tlp tor transmission trayer tree unzip usbutils
       valgrind vim_configurable virtmanager virtviewer vlock w3m weechat whois
-      wmname x11_ssh_askpass xbindkeys xcape xclip xdotool
-      xlockmore xml2 xournal xrestop xss-lock xtrlock-pam youtube-dl
-      zathura zip zsh-completions
+      wmname x11_ssh_askpass xlockmore xml2 xournal zathura zip zsh-completions
     ] ++ (with pkgs.xorg;[ # xorg
-      libXpm xbacklight xev xf86videointel xkbcomp xkill xprop
+      libXpm xbacklight xcape xclip xdotool xev xf86videointel
+      xkbcomp xkill xprop xrestop xss-lock xtrlock-pam
     ]) ++ (with pkgs.gnome3;[ # gnome
       baobab california cheese eog evince evolution gedit gnome-contacts
       gnome-control-center networkmanagerapplet
       gnome-documents gnome-online-accounts gnome-maps gnome-settings-daemon
       gnome-system-monitor gnome-tweak-tool nautilus
-    ]) ++ (with pkgs.aspellDicts;[ # dictionaries
+    ]) ++ (with pkgs.aspellDicts;[ # dictionaries and language tools
+      dico
       fr en-science en-computers es en de
     ]) ++ [ # Nix
       nix-bundle nix-index nix-prefetch-scripts nix-zsh-completions
-  # ] ++ [ # Debian
-  #   apt aptly debian-devscripts debianutils debootstrap dpkg
+    ] ++ [ # Debian
+      aptly debian-devscripts debianutils debootstrap dpkg dtools
     ] ++ (with python3Packages; [  # python
-      ipython parse requests tox virtualenv notebook ipykernel
-    ]) ++ [ # Electronics
-      pulseview
+      python3 ipython parse requests tox virtualenv
+    ]) ++ [ # Electronics & SDR
+      pulseview kicad
+      gnuradio-with-packages soapysdr-with-plugins
     ] ++ [ # sysadmin
-      lsof screen
+      python3Packages.glances lsof screen socat stow tcpdump
     ] ++ [ # drawing
       inkscape krita
     ] ++ [ # Triglav
-      arandr argyllcms adbfs-rootless enchant msmtp mymopidy tdesktop
+      arandr argyllcms adbfs-rootless enchant fortune msmtp mymopidy
+      screen-message tdesktop
       rxvt_unicode-with-plugins taffybar upower vlc wordnet tigervnc
     ] ++ [ # Dev
       ag arduino binutils platformio
       cmake ctags elfutils gcc gdb gnumake gpgme idris libgpgerror
       lua luaPackages.luacheck
-      sbt scala shellcheck
+      silver-searcher sbt scala shellcheck
       tig
     ]) ++ (with pkgs.haskellPackages; [
       ghc cabal-install
       xmobar # hsqml leksah
     ]) ++ (with bleedingEdge; [
-      gnuradio-with-packages
-      soapysdr-with-plugins
       mpv youtube-dl
     ]);
     shell = pkgs.zsh;
