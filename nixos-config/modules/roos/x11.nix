f858e2db60cc0a4cb3f6d44efd945dbf344c2d91
Roosembert Palacios <roosembert.palacios@epfl.ch>
2019-10-06 03:41:18 +0200

    nixos/modules: bump my X11 config
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- nixos-config/modules/roos/x11.nix
+++ nixos-config/modules/roos/x11.nix
@@ -5,34 +5,52 @@ with lib;
   options.roos.x11.enable = mkEnableOption "Roos' x11 config";
 
   config = mkIf config.roos.x11.enable {
-    services.xserver = {
-      enable = true;
-      layout = "us";
-      xkbVariant = "intl";
+    hardware = {
+      bumblebee.enable = true;
+      bumblebee.connectDisplay = true;
+      bumblebee.driver = "nvidia";
+      bumblebee.pmMethod = "bbswitch";
 
-      # Enable touchpad support.
-      libinput.enable = true;
+      opengl.enable = true;
+      opengl.driSupport = true;
+      opengl.driSupport32Bit = true;
+    };
 
-      displayManager.sessionCommands = ''
-        . $HOME/dotfiles/sh_environment
-        . $XDG_CONFIG_HOME/sh/profile
-        export XDG_CURRENT_DESKTOP=GNOME
-        ${pkgs.nitrogen}/bin/nitrogen --set-auto background-images/venice.png
-        ${pkgs.xcape}/bin/xcape -e 'Shift_L=Escape'
-        ${pkgs.xorg.setxkbmap}/bin/setxkbmap us intl -option caps:escape
-        ${pkgs.xorg.xrdb}/bin/xrdb $XDG_CONFIG_HOME/X11/Xresources
-        ${pkgs.xss-lock}/bin/xss-lock ${pkgs.xtrlock-pam}/bin/xtrlock-pam &!
-      '';
+    services = {
+      redshift.enable = true;
+      redshift.provider = "geoclue2";
 
-      displayManager.sddm.enable = true;
-      displayManager.sddm.enableHidpi = true;
-      windowManager.xmonad.enable = true;
-      windowManager.default = "xmonad";
-      windowManager.xmonad.extraPackages =
-        haskellPackages: with haskellPackages; [xmonad-contrib xmonad-extras];
-      desktopManager.default = "none";
-      desktopManager.gnome3.enable = true;
-      desktopManager.gnome3.debug = true;
+      xserver = {
+        enable = true;
+        layout = "us";
+        xkbVariant = "intl";
+
+        libinput.enable = true;  # Enable touchpad support.
+
+        displayManager.sessionCommands = ''
+          . $HOME/dotfiles/sh_environment
+          . $XDG_CONFIG_HOME/sh/profile
+          export XDG_CURRENT_DESKTOP=GNOME
+          ${pkgs.xcape}/bin/xcape -e 'Shift_L=Escape'
+          ${pkgs.xorg.setxkbmap}/bin/setxkbmap us intl -option caps:escape
+          ${pkgs.xorg.xrdb}/bin/xrdb $XDG_CONFIG_HOME/X11/Xresources
+          ${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.xtrlock-pam}/bin/xtrlock-pam -b none &!
+          ${pkgs.systemd}/bin/systemctl --user start random-background
+          ${pkgs.taffybar}/bin/taffybar &!
+        '';
+
+        displayManager.sddm.enable = true;
+        displayManager.sddm.enableHidpi = true;
+        windowManager.xmonad.enable = true;
+        windowManager.default = "xmonad";
+        windowManager.xmonad.extraPackages =
+          haskellPackages: with haskellPackages; [xmonad-contrib xmonad-extras taffybar];
+        desktopManager.default = "none";
+        desktopManager.gnome3.enable = true;
+        desktopManager.gnome3.debug = true;
+
+        videoDrivers = ["intel"];
+      };
     };
   };
 }
