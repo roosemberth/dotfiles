2c792a70b24c4c282d73025a6a521e45f27fd2ab
Roosembert Palacios <roosemberth@posteo.ch>
2020-03-10 19:19:24 +0100

    nixos/sandbox: Remove indicator-kdeconnect
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nixos-config/pkgs/sandbox.nix
+++ nixos-config/pkgs/sandbox.nix
@@ -14,47 +14,5 @@ in with nixpkgs;
 
     doCheck = false;
   };
-
-  indicator-kdeconnect = stdenv.mkDerivation rec {
-    name = "indicator-kdeconnect";
-    version = "0.9.4";
-
-    src = fetchFromGitHub {
-      owner = "bajoja";
-      repo = "indicator-kdeconnect";
-      rev = "${version}";
-      sha256 = "0ns05cm38mbhlh6xzdll921hxk4vl23lvjb21x056yivxafrqvl8";
-    };
-
-    nativeBuildInputs = with pkgs;[
-      pkgconfig
-      cmake
-      vala
-    ];
-
-    buildInputs = with pkgs;[
-      gnome3.libgee
-      gsettings-desktop-schemas
-      json-glib
-      libappindicator-gtk3
-
-      wrapGAppsHook
-    ];
-
-    propagatedBuildInputs = with python3.pkgs;[
-      requests_oauthlib
-      pygobject3
-    ] ++ [
-      gnome3.nautilus-python
-      kdeconnect
-      kde-cli-tools  # kcmshell5 needed to configure kdeconnect
-    ];
-
-    prePatch = ''
-      substituteInPlace src/DeviceIndicator.vala --replace '"kcmshell5"' '"${pkgs.kde-cli-tools}/bin/kcmshell5"'
-    '';
-
-    PKG_CONFIG_LIBNAUTILUS_EXTENSION_EXTENSIONDIR = "${placeholder "out"}/lib/nautilus/extensions-3.0";
-  };
   snooze = callPackage ./snooze {};
 }
