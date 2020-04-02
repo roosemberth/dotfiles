73fb9028ee5ced36b42fd772c8b201eccb6cc73a
Roosembert Palacios <roosemberth@posteo.ch>
2020-04-02 02:02:00 +0200

    nixos/ups/roosemberth: Application configuration overrides and xdg <3
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/user-profiles/roosemberth.nix
+++ nix/modules/user-profiles/roosemberth.nix
@@ -31,6 +31,40 @@ in
       ]);
     };
 
+    roos.rConfigFn = userCfg: let
+      homedir = userCfg.home.homeDirectory;
+    in {
+      xdg = {
+        enable = true;
+        cacheHome = "${homedir}/.local/var/cache";
+        configHome = "${homedir}/.local/etc";
+        dataHome = "${homedir}/.local/var/lib";
+        userDirs = {
+          enable = true;
+          download = "/tmp";
+          music = "$HOME/Media/Music";
+          pictures = "$HOME/Media/Pictures";
+          # Not backported yet
+          # publicShare = "$HOME/Public";
+          videos = "$HOME/Media/Videos";
+        };
+      };
+
+      home.sessionVariables = rec {
+        XDG_LIB_HOME = "$HOME/.local/lib";
+        XDG_LOG_HOME = "$HOME/.local/var/log";
+        XDF_RUNTIME_DIR = "\${XDG_RUNTIME_DIR:-/run/user/$(id -u)}";
+
+        ZDOTDIR = "${homedir}/ws/1-Repositories/dotfiles/local/etc/zsh/default";
+        GTK2_RC_FILES = "${userCfg.xdg.configHome}/gtk-2.0/gtkrc-2.0";
+        GTK_RC_FILES = "${userCfg.xdg.configHome}/gtk-1.0/gtkrc";
+
+        PASSWORD_STORE_DIR = "${XDG_LIB_HOME}/pass";
+        GNUPGHOME = "${XDG_LIB_HOME}/gnupg";
+        SSH_AUTH_SOCK = "${XDF_RUNTIME_DIR}/ssh-agent-$(id -un)-socket";
+      };
+    };
+
     roos.sConfig = {
       home.packages = (with pkgs; [
         bluezFull
