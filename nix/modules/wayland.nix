7ff431553d4ed358ddf12819d88d86fd95f829ed
Roosembert Palacios <roosemberth@posteo.ch>
2020-04-03 17:31:09 +0200

    Add mako configuration file and nix module support
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/wayland.nix
+++ nix/modules/wayland.nix
@@ -1,11 +1,17 @@
 { config, pkgs, lib, ... }: with lib;
-{
+let
+  util = import ./util.nix { inherit config pkgs lib; };
+in {
   options.roos.wayland.enable = mkEnableOption "Enable wayland support.";
 
   config = mkIf config.roos.wayland.enable {
-    roos.gConfig.home.sessionVariables = {
-      MOZ_ENABLE_WAYLAND = 1;
-      QT_QPA_PLATFORM = "wayland-egl";
+    roos.gConfig = {
+      home.sessionVariables = {
+        MOZ_ENABLE_WAYLAND = 1;
+        QT_QPA_PLATFORM = "wayland-egl";
+      };
+      home.packages = with pkgs; [ mako ];
+      xdg.configFile."".source = util.fetchDotfile "local/etc/mako/config";
     };
   };
 }
