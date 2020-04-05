ac9488313dbe8d212cc2d30568f7f3cf6f9b0ddc
Roosembert Palacios <roosemberth@posteo.ch>
2020-04-05 17:46:19 +0200

    nix wayland: start ssh with user session
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/wayland.nix
+++ nix/modules/wayland.nix
@@ -9,11 +9,22 @@ in {
       home.sessionVariables = {
         MOZ_ENABLE_WAYLAND = 1;
         QT_QPA_PLATFORM = "wayland-egl";
-        SSH_ASKPASS="${pkgs.x11_ssh_askpass}/libexec/ssh-askpass";
       };
       home.packages = with pkgs; [ sway mako ];
       xdg.configFile."mako/config".source = util.fetchDotfile "etc/mako/config";
       xdg.configFile."sway/config".source = util.fetchDotfile "etc/sway/config";
+
+      systemd.user.services.ssh-agent = {
+        Unit.Description = "SSH Agent";
+        Unit.PartOf = [ "graphical-session.target" ];
+        Install.WantedBy = [ "graphical-session.target" ];
+        Service = {
+          ExecStart = "${pkgs.openssh}/bin/ssh-agent -D -a %t/ssh-agent-%u-socket";
+          Restart = "always";
+          RestartSec = "3";
+          Environment = [ "SSH_ASKPASS=${pkgs.x11_ssh_askpass}/libexec/ssh-askpass" ];
+        };
+      };
     };
   };
 }
