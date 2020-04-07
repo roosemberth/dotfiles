27182307c40886d5c98d375687f40f5c45bbe4a4
Roosembert Palacios <roosemberth@posteo.ch>
2020-04-08 01:37:31 +0200

    agenda.nix: tweak config for module afew
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/agenda.nix
+++ nix/modules/agenda.nix
@@ -13,22 +13,36 @@ in {
         accounts = flip mapAttrs emailAccounts (_: secretCfg: let
           accountCfg = filterAttrs (n: _: n != "passwordPath") secretCfg;
         in accountCfg // {
-          passwordCommand = "${pkgs.pass}/bin/pass show ${secretCfg.passwordPath}";
+          passwordCommand =
+            "${pkgs.pass}/bin/pass show ${secretCfg.passwordPath}";
         });
         maildirBasePath = ".local/var/mail";
       };
-      home.packages = with pkgs; [ gnupg mailcap pass-otp taskwarrior timewarrior ];
+      home.packages = with pkgs;
+        [ gnupg mailcap pass-otp taskwarrior timewarrior ];
       home.sessionVariables = rec {
         PASSWORD_STORE_DIR = "${userCfg.xdg.dataHome}/pass";
         GNUPGHOME = "${userCfg.xdg.dataHome}/gnupg";
       };
 
       programs.afew.enable = true;
+      programs.afew.extraConfig = ''
+        [FolderNameFilter]
+        # Intentionally lie to the filter to prevent it from flattening
+        # the directory structure
+        maildir_separator = .
+        [KillThreadsFilter]
+        [ListMailsFilter]
+        [SpamFilter]
+        [InboxFilter]
+
+      '';
       programs.alot.enable = true;
       programs.mbsync.enable = true;
       programs.notmuch.enable = true;
       programs.notmuch.hooks.postNew = "afew --tag --new";
       programs.notmuch.hooks.preNew = "mbsync --all";
+      programs.notmuch.new.tags = [ "new" ];
 
       xdg.configFile."task/taskrc".source = util.fetchDotfile "etc/taskrc";
     };
