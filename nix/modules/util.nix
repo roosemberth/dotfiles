19435308d863f4bfb026a2044f2beb92bafae0df
Roosembert Palacios <roosemberth@posteo.ch>
2020-04-12 18:35:36 +0200

    nix modules util: add function renderDotfile
    From it's docstring:
    
    Replaces every occurrence of @varName@ in a copy of every file in path,
    where varName is any variable in the specified attr set. If path is a file
    only that file will be copied and processed.
    The variable @destDir@ will be automatically added with value equal to the
    derivation path.
    Variables may start with lowercase and contain only alphabetic characters.
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/util.nix
+++ nix/modules/util.nix
@@ -1,9 +1,29 @@
-{ config, pkgs, lib, ...}:
-{
+{ config, pkgs, lib, ...}: with builtins;
+let
+  dotfiles = assert config.roos.dotfilesPath != null; config.roos.dotfilesPath;
+in {
   fetchDotfile = target: let
     impure = config.roos.impureDotfiles;
-    dotfiles = assert config.roos.dotfilesPath != null; config.roos.dotfilesPath;
-  in pkgs.runCommandNoCCLocal "${if impure then "impure-" else ""}dotfile-path" {} ''
-    ln -s "${if impure then builtins.toString dotfiles else dotfiles}/${target}" "$out"
+    name = "${if impure then "impure-" else ""}dotfile-path";
+  in pkgs.runCommandNoCCLocal name {} ''
+    ln -s "${if impure then toString dotfiles else dotfiles}/${target}" "$out"
+  '';
+
+  /* Replaces every occurrence of @varName@ in a copy of every file in path,
+   * where varName is any variable in the specified attr set. If path is a file
+   * only that file will be copied and processed.
+   * The variable @destDir@ will be automatically added with value equal to the
+   * derivation path.
+   * Variables may start with lowercase and contain only alphabetic characters.
+   */
+  renderDotfile = path: attrs: let
+    dotfile = pkgs.copyPathToStore "${toString dotfiles}/${path}";
+  in pkgs.runCommand "rendered-dotfile-path" ({
+    destDir = placeholder "out";
+  } // attrs) ''
+    cp -r "${dotfile}" $out
+    find "$out" -type f | while read -r file; do
+       substituteAllInPlace "$file"
+    done
   '';
 }
