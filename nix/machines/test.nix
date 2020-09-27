f7f82ca80c703656264c1bfaa1b6d2457bed55b5
Roosembert Palacios <roosemberth@posteo.ch>
2020-09-27 12:24:50 +0200

    nix: Add test VM prototype
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

---
+++ nix/machines/test.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, secrets, ... }:
+let
+  hostname = config.networking.hostName;
+in
+{
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+
+  fileSystems."/boot" = { fsType = "vfat"; device = "/dev/sda1"; };
+  fileSystems."/".device = "/dev/sda2";
+
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  services.journald.console = "/dev/console";
+  services.mingetty.autologinUser = "roos";
+  systemd.coredump.enable = true;
+  security.pam.loginLimits = [{
+    domain = "*"; item = "core"; type = "-"; value = "-1";
+  }];
+
+  users.mutableUsers = false;
+  users.extraUsers.roos.password = "roos";
+  users.extraUsers.roos.isNormalUser = true;
+  users.extraUsers.roos.extraGroups = ["wheel"];
+}
