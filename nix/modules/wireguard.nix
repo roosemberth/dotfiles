23ed7d78d15174e481486f9a0a0c384b99159b75
Roosembert Palacios <roosemberth@posteo.ch>
2020-10-03 22:44:28 +0200

    wireguard.nix: ripple from refactoring network secrets
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/wireguard.nix
+++ nix/modules/wireguard.nix
@@ -3,16 +3,18 @@ let
   cfg = config.roos.wireguard;
   cidrToHost = address:
     builtins.replaceStrings ["/24" "/120"] ["/32" "/128"] address;
-  netsForHost = host:
-    flatten (attrValues (attrByPath [host] {} secrets.network.foreignNetworks));
   mkWireguardPeer = host: endpoint: with secrets; with network; {
     inherit endpoint;
-    allowedIPs = map cidrToHost hostNetworks.${host} ++ netsForHost host;
+    allowedIPs = with zkx.${host}; map cidrToHost [host4 host6] ++ ipv4 ++ ipv6;
     persistentKeepalive = 30;
     publicKey = (forHost host).keys.wireguard.public;
   };
-  wireguardPeers =
-    mapAttrs mkWireguardPeer secrets.network.publicWireguardEndpoints;
+  bastions = mapAttrs mkWireguardPeer secrets.network.zkxBastions;
+  allNetworkIPs = flatten (
+    mapAttrsToList
+      (_: p: with p; [host4 host6] ++ ipv4 ++ ipv6)
+      secrets.network.zkx
+  );
 in {
   options.roos.wireguard = {
     enable = mkEnableOption "Enable wireguard.";
@@ -32,29 +34,29 @@ in {
         If specified, such peer will be used to connect to the network.
         No other peers will be added to the VPN configuration.
         If null, all peers will be added with their respective networks
-        as defined in secrets.network.hostNetworks.
+        as defined in secrets.network.zkx.
       '';
     };
   };
 
   config = let
     hostname = config.networking.hostName;
-    endpoints = secrets.network.publicWireguardEndpoints;
+    endpoints = secrets.network.zkxBastions;
     listenPort = if !hasAttr hostname endpoints then 61573
       else head (tail (strings.splitString ":" endpoints.${hostname}));
     gwServerAssert = assertMsg (hasAttr cfg.gwServer endpoints)
-                       "The specified gwServer is unkonwn.";
+                       "The specified bastion is unknown.";
   in mkIf cfg.enable {
     networking.firewall.allowedUDPPorts = [ listenPort ];
     networking.firewall.trustedInterfaces = [ cfg.interface ];
     networking.wireguard.interfaces.${cfg.interface} =
       if !secrets.secretsAvailable then {} else {
         inherit listenPort;
-        ips = secrets.network.hostNetworks.${hostname};
+        ips = with secrets.network.zkx.${hostname}; [host4 host6];
         privateKey = (secrets.forHost hostname).keys.wireguard.private;
-        peers = if cfg.gwServer == null then attrValues wireguardPeers
-          else assert gwServerAssert; [(wireguardPeers.${cfg.gwServer} // {
-            allowedIPs = flatten (attrValues secrets.network.hostNetworks);
+        peers = if cfg.gwServer == null then attrValues bastions
+          else assert gwServerAssert; [(bastions.${cfg.gwServer} // {
+            allowedIPs = allNetworkIPs;
           })];
         allowedIPsAsRoutes = false;
       };
