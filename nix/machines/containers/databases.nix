a2c536c67ecd29c9b6fa2ed2957cb1f142c89c56
Roosembert Palacios <roosemberth@posteo.ch>
2020-11-03 19:21:20 +0100

    nix: Use ensure-nodatacow-btrfs-subvolme package
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/containers/databases.nix
+++ nix/machines/containers/databases.nix
@@ -36,43 +36,8 @@
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = pkgs.writeShellScript "ensure-nodatacow-btrfs-subvolme" ''
-        # Ensures the specified target exists as a btrfs subvolume and has
-        # the No_COW attribute set.
-        # If the specified TARGET does not exist, it is created as a btrfs
-        # subvolume and the No_COW attribute is set.
-
-        if [ -z "$TARGET" ]; then
-          echo "Missing required environment variable 'TARGET'." >&2
-          exit 1
-        fi
-
-        EXISTING_TARGET_PREFIX="$TARGET"
-        while ! [ -d "$EXISTING_TARGET_PREFIX" ]; do
-          EXISTING_TARGET_PREFIX="$(dirname "$EXISTING_TARGET_PREFIX")"
-        done
-
-        if ! [ "btrfs" = "$(findmnt -DnT "$EXISTING_TARGET_PREFIX" | awk '{print $2}')" ]; then
-          echo "The specified 'TARGET' does not reside inside a btrfs mountpoint." >&2
-          exit 2
-        fi
-
-        MOUNT_POINT="$(findmnt -DnT "$EXISTING_TARGET_PREFIX" | awk '{print $NF}')"
-        # Path of the TARGET inside the btrfs mount point.
-        SUBPATH="''${TARGET#$MOUNT_POINT/}"
-
-        # Test if the target is already a btrfs subvolume.
-        if ! btrfs subvolume list $MOUNT_POINT | grep -Eq "$SUBPATH\$"; then
-          # Make sure the parent directory exists
-          mkdir -p "$(dirname "$TARGET")"
-          btrfs subvolume create "$TARGET"
-          chattr -R +C "$TARGET"
-        elif ! [ "$(lsattr -d "$TARGET" | cut -c16)" = "C" ]; then
-          echo "The specified 'TARGET' exists as a btrfs subvolume but " \
-               "does not have the No_COW attribute set." >&2
-          exit 3
-        fi
-      '';
+      ExecStart = let tool = "${pkgs.ensure-nodatacow-btrfs-subvolume}";
+      in "${tool}/bin/ensure-nodatacow-btrfs-subvolume";
     };
   };
 }
