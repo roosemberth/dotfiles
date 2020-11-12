2972b7c5a9147b355e8c1b66194cf6e6becd6594
Roosembert Palacios <roosemberth@posteo.ch>
2020-11-12 22:43:47 +0100

    pkgs/overlay: Add matrix-federation-tester
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/pkgs/overlay.nix
+++ nix/pkgs/overlay.nix
@@ -2,4 +2,21 @@ final: prev: {
   mfgtools = final.callPackage ./mfgtools { };
   ensure-nodatacow-btrfs-subvolume =
     final.callPackage ./ensure-nodatacow-btrfs-subvolume.nix { };
+  matrix-federation-tester = with final; buildGoModule rec {
+    pname = "matrix-federation-tester";
+    version = "0.2";
+    src = fetchFromGitHub {
+      owner = "matrix-org";
+      repo = "matrix-federation-tester";
+      rev = "v${version}";
+      sha256 = "sha256-Z1/2hWHsmLtOeHUgr/Jr0FF8WRsbUGWaKbiqTdSvKDU=";
+    };
+    vendorSha256 = "sha256-1zbNYB6S72/uzoeUMdaCQjzKOZ1xTlGc3oseXWGyetA=";
+    meta = with lib; {
+      description = "Tester for matrix federation written in golang";
+      homepage = "https://github.com/matrix-org/matrix-federation-tester";
+      maintainers = with maintainers; [ roosemberth ];
+      platforms = platforms.linux;
+    };
+  };
 }
