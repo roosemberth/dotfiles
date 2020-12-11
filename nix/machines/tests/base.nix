3eba087fde8a61905e0aa8b21a3b2c54cdcf45a4
Roosembert Palacios <roosemberth@posteo.ch>
2020-12-11 03:01:35 +0100

    WIP batman test: Simple manual nested OK
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/tests/base.nix
+++ nix/machines/tests/base.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, secrets, ... }:
 {
+  imports = [
+    /nix/store/w2j80xzcshl55drf24vcr5gvw3cmiycr-source/nixos/modules/virtualisation/qemu-vm.nix
+  ];
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
@@ -24,4 +27,13 @@
   users.extraUsers.roos.password = "roos";
   users.extraUsers.roos.isNormalUser = true;
   users.extraUsers.roos.extraGroups = ["wheel"];
+
+  virtualisation.qemu.options = [
+    "-device virtio-balloon-pci,id=balloon0,bus=pci.0"
+    "-chardev stdio,mux=on,id=char0,signal=off"
+    "-mon chardev=char0,mode=readline"
+    "-serial chardev:char0"
+    "-snapshot"
+    "-nographic"
+  ];
 }
