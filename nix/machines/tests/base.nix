a92a94c357787950c918be7d2aa3923f9583c0c0
Roosembert Palacios <roosemberth@posteo.ch>
2021-02-23 13:29:42 +0100

    tests/base: Add configuration option to enable graphics
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/tests/base.nix
+++ nix/machines/tests/base.nix
@@ -1,52 +1,55 @@
 { config, lib, pkgs, secrets, modulesPath, ... }:
 {
   imports = [ "${modulesPath}/virtualisation/qemu-vm.nix" ];
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
+
+  options.virtualisation.enableGraphics = lib.mkEnableOption "Graphics support";
+  config = {
+    boot.loader.grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+    environment.etc."systemd/network/00-random-mac.link".text = ''
+      [Match]
+      OriginalName=*
+
+      [Link]
+      MACAddressPolicy=random
+    '';
+
+    fileSystems."/boot" = { fsType = "vfat"; device = "/dev/sda1"; };
+    fileSystems."/".device = "/dev/sda2";
+
+    networking.firewall.allowedUDPPorts = [ 5355 ];
+    networking.interfaces.eth0.useDHCP = true;
+    networking.useDHCP = false;
+    networking.useNetworkd = true;
+
+    nix.package = pkgs.nixUnstable;
+    nix.trustedUsers = [ "roos" ];
+
+    security.sudo.enable = true;
+    security.sudo.wheelNeedsPassword = false;
+
+    services.journald.console = "/dev/ttyS0";
+    services.mingetty.autologinUser = "roos";
+    systemd.coredump.enable = true;
+    security.pam.loginLimits = [{
+      domain = "*"; item = "core"; type = "-"; value = "-1";
+    }];
+
+    users.mutableUsers = false;
+    users.users.roos.password = "roos";
+    users.users.roos.isNormalUser = true;
+    users.users.roos.extraGroups = ["wheel"];
+
+    virtualisation.qemu.options = [
+      "-device virtio-balloon-pci,id=balloon0,bus=pci.0"
+      "-chardev stdio,mux=on,id=char0,signal=off"
+      "-mon chardev=char0,mode=readline"
+      "-serial chardev:char0"
+      "-snapshot"
+    ] ++ lib.optional (!config.virtualisation.enableGraphics) "-nographic";
   };
-  environment.etc."systemd/network/00-random-mac.link".text = ''
-    [Match]
-    OriginalName=*
-
-    [Link]
-    MACAddressPolicy=random
-  '';
-
-  fileSystems."/boot" = { fsType = "vfat"; device = "/dev/sda1"; };
-  fileSystems."/".device = "/dev/sda2";
-
-  networking.firewall.allowedUDPPorts = [ 5355 ];
-  networking.interfaces.eth0.useDHCP = true;
-  networking.useDHCP = false;
-  networking.useNetworkd = true;
-
-  nix.package = pkgs.nixUnstable;
-  nix.trustedUsers = [ "roos" ];
-
-  security.sudo.enable = true;
-  security.sudo.wheelNeedsPassword = false;
-
-  services.journald.console = "/dev/ttyS0";
-  services.mingetty.autologinUser = "roos";
-  systemd.coredump.enable = true;
-  security.pam.loginLimits = [{
-    domain = "*"; item = "core"; type = "-"; value = "-1";
-  }];
-
-  users.mutableUsers = false;
-  users.users.roos.password = "roos";
-  users.users.roos.isNormalUser = true;
-  users.users.roos.extraGroups = ["wheel"];
-
-  virtualisation.qemu.options = [
-    "-device virtio-balloon-pci,id=balloon0,bus=pci.0"
-    "-chardev stdio,mux=on,id=char0,signal=off"
-    "-mon chardev=char0,mode=readline"
-    "-serial chardev:char0"
-    "-snapshot"
-    "-nographic"
-  ];
 }
