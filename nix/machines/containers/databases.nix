c477bedeb46c4d697c32c1d0e4b97228669489e4
Roosembert Palacios <roosemberth@posteo.ch>
2021-03-17 00:23:38 +0100

    Add InfluxDB and Grafana to Minerva/databases
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/containers/databases.nix
+++ nix/machines/containers/databases.nix
@@ -1,9 +1,16 @@
-{ config, pkgs, secrets, ... }:
-{
+{ config, pkgs, secrets, ... }: let
+  removeCIDR = ip: builtins.head (builtins.split "/" ip);
+in {
   containers.databases = {
     autoStart = true;
     bindMounts.psql-data.mountPoint = "/var/lib/postgresql";
     bindMounts.psql-data.isReadOnly = false;
+    bindMounts.influxdb.hostPath = "/mnt/cabinet/minerva-data/influxdb";
+    bindMounts.influxdb.mountPoint = "/var/db/influxdb";
+    bindMounts.influxdb.isReadOnly = false;
+    bindMounts.grafana.hostPath = "/mnt/cabinet/minerva-data/grafana";
+    bindMounts.grafana.mountPoint = "/var/lib/grafana";
+    bindMounts.grafana.isReadOnly = false;
     config = {
       services.postgresql = {
         enable = true;
@@ -14,11 +21,31 @@
         '';
         settings.log_connections = true;
       };
+      services.influxdb = {
+        enable = true;
+        extraConfig = {
+          http.auth-enabled = true;
+          http.bind-address = ":39425";
+        };
+      };
+      # This database is only available from minerva.
+      services.grafana = assert config.networking.hostName == "Minerva"; {
+        enable = true;
+        addr = removeCIDR secrets.network.zkx.Minerva.host4;
+        domain = "minerva.int";
+        provision.enable = true;
+        provision.datasources = secrets.zkx.minerva-grafana-sources;
+      };
     };
     ephemeral = true;
-    forwardPorts = [ { hostPort = 5432; protocol = "tcp"; } ];
+    forwardPorts = [
+      { hostPort = 3000; protocol = "tcp"; }
+      { hostPort = 39425; protocol = "tcp"; }
+      { hostPort = 5432; protocol = "tcp"; }
+    ];
   };
 
+  networking.firewall.allowedTCPPorts = [ 39425 ];
   networking.search = with secrets.network.zksDNS; [ search ];
   networking.nameservers = with secrets.network.zksDNS; v6 ++ v4;
 
