f73588b8806d43e2755553cf3f6543997b6e744d
Roosembert Palacios <roosemberth@posteo.ch>
2021-04-29 14:05:25 +0200

    Minerva: Remove kernel version override
    We were previously using linuxPackages_latest since btrfs xxhash support
    requires version >=5.5. Current nixpkgs target uses kernel 5.10.
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/Minerva.nix
+++ nix/machines/Minerva.nix
@@ -18,10 +18,6 @@ in {
     networkDnsConfig
   ];
 
-  # btrfs xxhash support requires >=5.5
-  boot.kernelPackages = assert lib.versionOlder pkgs.linux.version "5.6";
-    (lib.mkDefault pkgs.linuxPackages_latest);
-
   boot.cleanTmpDir = true;
   boot.kernel.sysctl."kernel.yama.ptrace_scope" = 2;  # Enable YAMA restrictions
   boot.kernelModules = [ "kvm-intel" ];
