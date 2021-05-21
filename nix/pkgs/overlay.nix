efbe159f2e508be1ef8bce409b05ac2397ec7b5b
Roosembert Palacios <roosemberth@posteo.ch>
2021-05-21 17:40:13 +0200

    nix/pkgs overlay: Use git version of alot
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/pkgs/overlay.nix
+++ nix/pkgs/overlay.nix
@@ -98,4 +98,16 @@ final: prev: {
   };
   nvim-roos = final.callPackage ./nvim-roos {};
   mopidy-roos = final.callPackage ./mopidy {};
+  alot = assert final.lib.versionAtLeast "0.9.1" prev.alot.version;
+    prev.alot.overrideAttrs(o: {
+      version = "0.9.1-git";
+      src = final.fetchFromGitHub {
+        owner = "pazz";
+        repo = "alot";
+        rev = "a814a7744e0e7d98656980fcdae3ed712a299d8e";
+        hash = "sha256-UsWsWGr1zgY4KpSK9bgzXhW6fdqIWTa5A0kddxhyfAs=";
+      };
+      propagatedBuildInputs = o.propagatedBuildInputs or [] ++
+        (with prev.python3Packages; [notmuch2]);
+    });
 }
