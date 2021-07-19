f65c1fc9daad5cb59820909cc2d587d0befd6675
Roosembert Palacios <roosemberth@posteo.ch>
2021-07-19 20:42:58 +0200

    wm-compat.nix: Add sane qemu defaults to make my file easier
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/vm-compat.nix
+++ nix/modules/vm-compat.nix
@@ -3,13 +3,24 @@
 #
 # More importantly, this file should not be imported from the system
 # derivation.
-{ lib, ... }: with lib;
+{ lib, modulesPath, ... }: with lib;
 {
+  imports = [ "${modulesPath}/virtualisation/qemu-vm.nix" ];
+
   config = {
     # Fails assertion because of missing btrfs filesystems
     services.btrfs.autoScrub.enable = mkVMOverride false;
 
     # Avoid mishaphs
     roos.wireguard.enable = mkVMOverride false;
+
+    virtualisation.qemu.options = [
+      "-device virtio-balloon-pci,id=balloon0,bus=pci.0"
+      "-chardev stdio,mux=on,id=char0,signal=off"
+      "-mon chardev=char0,mode=readline"
+      "-serial chardev:char0"
+      "-snapshot"
+      "-vga cirrus"
+    ];
   };
 }
