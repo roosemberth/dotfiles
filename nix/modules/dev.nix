f2c57be6ab7a580fedd50973c3cfb1e98a89bd39
Roosembert Palacios <roosemberth@posteo.ch>
2021-09-15 00:53:46 +0200

    Add stylish-haskell config file
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/dev.nix
+++ nix/modules/dev.nix
@@ -1,50 +1,56 @@
-{ config, pkgs, lib, ... }: with lib;
-{
+{ config, pkgs, lib, ... }: with lib; let
+  util = import ./util.nix { inherit config pkgs lib; };
+in {
   options.roos.dev.enable = mkEnableOption "Install language development packages";
 
   config = mkIf config.roos.dev.enable {
-    roos.sConfig.services.lorri.enable = true;
-    roos.sConfig.home.packages = with pkgs; [
-      # C/C++
-      clang
-      clang-tools
-      cmake
-      ctags
-      gnumake
-      # Haskell
-      (ghc.withHoogle (p: with p; [
-        QuickCheck
-        aeson
-        generic-arbitrary
-        optparse-applicative
-        parsec
-        protolude
-        quickcheck-instances
-        yaml
-      ]))
-      haskellPackages.fast-tags
-      haskell-language-server
-      stack
-      # Node
-      nodejs
-      yarn
-      # Python
-      python3Packages.black
-      # Embedded
-      platformio
-      # Other
-      httpie
-      wdiff
-      jq
-      yq
-      virt-viewer
-      flutter
-      claws
-      # lorri
-      direnv
-      # Nix
-      manix
-    ];
+    roos.sConfig = {
+      services.lorri.enable = true;
+      home.packages = with pkgs; [
+        # C/C++
+        clang
+        clang-tools
+        cmake
+        ctags
+        gnumake
+        # Haskell
+        (ghc.withHoogle (p: with p; [
+          QuickCheck
+          aeson
+          generic-arbitrary
+          optparse-applicative
+          parsec
+          protolude
+          quickcheck-instances
+          yaml
+        ]))
+        haskellPackages.fast-tags
+        haskell-language-server
+        stack
+        # Node
+        nodejs
+        yarn
+        # Python
+        python3Packages.black
+        # Embedded
+        platformio
+        # Other
+        httpie
+        wdiff
+        jq
+        yq
+        virt-viewer
+        flutter
+        claws
+        # lorri
+        direnv
+        # Nix
+        manix
+      ];
+
+      xdg.configFile."stylish-haskell/config.yaml".source =
+        util.fetchDotfile "etc/stylish-haskell.yaml";
+    };
 
     services.udev.extraRules = ''
       # Remarkable tablet upload mode (I.MX6)
