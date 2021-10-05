ffbe91472009645d9b45b49fba32e58160eea9f0
Roosembert Palacios <roosemberth@posteo.ch>
2021-10-05 23:42:42 +0200

    Remove manually packaged version of wshowkeys
    It's now upstream!
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/pkgs/overlay.nix
+++ nix/pkgs/overlay.nix
@@ -22,18 +22,6 @@ in {
       platforms = platforms.linux;
     };
   };
-  wshowkeys = with final; stdenv.mkDerivation {
-    name = "wshowkeys";
-    version = "0.0-git";
-    nativeBuildInputs = with pkgs; [ninja meson pkgconfig cmake];
-    buildInputs = with pkgs; [
-      cairo libinput pango udev wayland libxkbcommon wayland-protocols
-    ];
-    src = fetchGit {
-      url = "https://git.sr.ht/~sircmpwn/wshowkeys";
-      rev = "6388a49e0f431d6d5fcbd152b8ae4fa8e87884ee";
-    };
-  };
   wl-clipboard-x11 = with final; stdenv.mkDerivation rec {
     name = "wl-clipboard-x11";
     version = "5";
