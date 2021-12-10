91f862b3902ab61f6cd653f389d3364b2fa76c59
Roosembert Palacios <roosemberth@posteo.ch>
2021-12-10 06:05:49 +0100

    Heimdaalr: Setup ACME using DNS challenge for orbstheorem.ch
    Uses RFC2136 to update DNS records in Bind.
    A temporary file is used as zone file because bind needs to be able to
    write to the file upon receiving an update.
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/Heimdaalr.nix
+++ nix/machines/Heimdaalr.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }: let
-  bindConfig = { secrets, ... }: {
+  bindConfig = { secrets, ... }: let
+    zone."orbstheorem.ch" = "/run/bind/orbstheorem.ch.zone";
+  in {
     networking.firewall.allowedUDPPorts = [53];
     services.bind = {
       enable = true;
@@ -9,18 +11,42 @@
       zones = [{
         name = "orbstheorem.ch";
         master = true;
-        file = secrets.network.bind-zones."orbstheorem.ch";
+        # Writeable so it can be updated during acme provisioning via rfc2136.
+        file = zone."orbstheorem.ch";
         extraConfig = "allow-update { key rfc2136key.orbstheorem.ch.; };";
       }];
     };
+    systemd.services.bind.preStart = let
+      srcfile = secrets.network.bind-zones."orbstheorem.ch";
+      cfgfile = pkgs.writeText "Replace orbstheorem.ch zone file.tmpfiles" ''
+        d /run/bind 0700 named root 0
+        C ${zone."orbstheorem.ch"} 0400 named root - ${srcfile}
+      '';
+    in ''
+      ${pkgs.systemd}/bin/systemd-tmpfiles --create --remove "${cfgfile}"
+    '';
     systemd.tmpfiles.rules = [
       "f /keyring/dns/dns-orbstheorem.ch.keys.conf 0400 named root -"
+    ];
+  };
+  acmeConfig = { secrets, ... }: {
+    security.acme.acceptTerms = true;
+    security.acme.email = secrets.network.acme.email;
+    security.acme.certs."orbstheorem.ch" = {
+      domain = "orbstheorem.ch";
+      dnsProvider = "rfc2136";
+      credentialsFile = "/keyring/acme/orbstheorem.ch.secret";
+      dnsPropagationCheck = false;
+    };
+    systemd.tmpfiles.rules = [
+      "f /keyring/acme/orbstheorem.ch.secret 0400 acme root -"
     ];
   };
 in {
   imports = [
     ../modules
     ./Heimdaalr-static.nix
+    acmeConfig
     bindConfig
   ];
 
