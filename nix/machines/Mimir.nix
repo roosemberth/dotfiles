3b4788fafa3e58781dfaa19c6eb9f7ea5010c9af
Roosembert Palacios <roosemberth@posteo.ch>
2022-04-10 00:09:53 +0200

    Mimir: Add amd opengl and vulkan drivers
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/Mimir.nix
+++ nix/machines/Mimir.nix
@@ -53,8 +53,10 @@ in {
     cpu.intel.updateMicrocode = true;
 
     opengl.enable = true;
-    opengl.extraPackages = with pkgs;
-      [ vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver ];
+    opengl.extraPackages = with pkgs; [
+      vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver
+      amdvlk rocm-opencl-icd
+    ];
 
     pulseaudio.enable = false;
   };
