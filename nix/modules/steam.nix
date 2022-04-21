42ea6fe5e9860c873f8127de727a2a289e95290a
Roosembert Palacios <roosemberth@posteo.ch>
2022-04-21 15:07:26 +0200

    steam.nix: Remove SDL_VIDEODRIVER override
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/steam.nix
+++ nix/modules/steam.nix
@@ -1,20 +1,9 @@
 { config, pkgs, lib, ... }: with lib;
-let
-  steam' = (pkgs.steam.override {
-    extraPkgs = p: with p; [];
-  }).overrideAttrs (o: {
-    postFixup = o.postFixup or "" + ''
-      for f in $(find $out/bin/ $out/libexec/ -type f -executable); do
-        wrapProgram "$f" --set SDL_VIDEODRIVER x11
-      done
-    '';
-  });
-in
 {
   options.roos.steam.enable = mkEnableOption "Enable steam support";
 
   config = mkIf config.roos.steam.enable {
-    roos.gConfig.home.packages = [steam' steam'.run];
+    roos.gConfig.home.packages = with pkgs; [steam steam.run];
 
     hardware.opengl.driSupport32Bit = true;
     hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
