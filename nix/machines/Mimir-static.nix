b6e11c601bf3ad309c88486a292de217d6b549e9
Roosembert Palacios <roos@Mimir>
2022-05-21 17:33:37 +0200

    Attempt to implement ephemeral mount points

--- nix/machines/Mimir-static.nix
+++ nix/machines/Mimir-static.nix
@@ -13,7 +13,39 @@ in
     initrd = {
       availableKernelModules = [ "xhci_pci" "nvme" "sd_mod" ];
       kernelModules = ["dm_crypt" "cbc" "kvm-intel" "e1000e"];
-      luks.devices."${hostname}".device = "/dev/disk/by-partuuid/${uuids.systemDevice}";
+      luks.devices."${hostname}" = {
+        device = "/dev/disk/by-partuuid/${uuids.systemDevice}";
+        postOpenCommands = ''
+          mkdir "/tmp-${hostname}-root"
+          mount -t btrfs "/dev/mapper/${hostname}" "/tmp-${hostname}-root"
+          cd "/tmp-${hostname}-root/subvolumes/ephemeral"
+
+          EPH_VERSION="$(date -u +%y%m%dZ%H%M)"
+          mkdir -p "$EPH_VERSION"
+          for v in $(ls -v1 templates); do  # Generate ephemeral volumes
+            btrfs subvolume snapshot "templates/$v" "$EPH_VERSION/$v"
+          done
+          ln -sf "$EPH_VERSION" active
+
+          for v in $(ls -v1 | grep -v templates); do
+            if ! date -d "$v" &> /dev/null; then
+              echo -n "WARNING: Could not determine whether to clean up " >&2
+              echo "ephemeral version $v" >&2
+              continue
+            fi
+            if [ $(date -d "$v" +%s) -lt $(date 'now - 7 days' +%s) ]; then
+              echo "Deleting old ephemeral system version $v"
+              for sv in $(ls -v1 "$v"); do
+                btrfs subvolume delete "$v/$sv"
+              done
+              rm -fr "$v"
+            fi
+          done
+
+          cd
+          umount "/tmp-${hostname}-root"
+        '';
+      };
       supportedFilesystems = [ "btrfs" "ext4" ];
     };
     kernelModules = ["acpi_call"];
@@ -31,39 +63,57 @@ in
 
   hardware.enableRedistributableFirmware = true;
 
-  #swapDevices = [
-  #  { device = "/mnt/root-btrfs/subvolumes/swap/swapfile1"; }
-  #];
+  swapDevices = [
+    { device = "/mnt/root-btrfs/subvolumes/swap/swapfile1"; }
+  ];
 
   fileSystems = {
-    "/" = {
-      fsType = "btrfs";
-      mountPoint = "/";
-      device = "/dev/mapper/" + hostname;
-      options = ["subvol=/subvolumes/tmp/rootfs" "compress=zlib" "user_subvol_rm_allowed"];
-    };
     "/boot" = {
       fsType = "vfat";
       mountPoint = "/boot";
       device = "/dev/disk/by-partuuid/${uuids.bootPart}";
     };
-    "/var" = {
-      fsType = "btrfs";
-      mountPoint = "/var";
-      device = "/dev/mapper/" + hostname;
-      options = ["subvol=/subvolumes/tmp/var" "compress=zlib" "user_subvol_rm_allowed"];
-    };
     "/nix" = {
       fsType = "btrfs";
       mountPoint = "/nix";
       device = "/dev/mapper/" + hostname;
-      options = ["subvol=/subvolumes/persisted/@nix" "compress=zlib" "defaults" "noatime" "autodefrag" "nodatacow"];
+      options = [
+        "compress=zlib"
+        "defaults"
+        "noatime"
+        "nodatacow"
+        "subvol=/subvolumes/persisted/@nix"
+      ];
+    };
+    "/mnt/root-btrfs" = {
+      fsType = "btrfs";
+      mountPoint = "/mnt/root-btrfs";
+      device = "/dev/mapper/" + hostname;
+      options = ["nodatacow" "noatime" "noexec" "user_subvol_rm_allowed"];
+    };
+
+    # Ephemeral volumes
+    "/" = {
+      fsType = "btrfs";
+      mountPoint = "/";
+      device = "/dev/mapper/" + hostname;
+      options = ["subvol=/subvolumes/ephemeral/active/rootfs" "compress=zlib"];
     };
     "/home" = {
       fsType = "btrfs";
       mountPoint = "/home";
       device = "/dev/mapper/" + hostname;
-      options = ["subvol=/subvolumes/tmp/home" "compress=zlib" "autodefrag" "user_subvol_rm_allowed"];
+      options = [
+        "subvol=/subvolumes/ephemeral/active/home"
+        "compress=zlib"
+        "user_subvol_rm_allowed"
+      ];
+    };
+    "/var" = {
+      fsType = "btrfs";
+      mountPoint = "/var";
+      device = "/dev/mapper/" + hostname;
+      options = ["subvol=/subvolumes/ephemeral/active/var" "compress=zlib"];
     };
   };
 
