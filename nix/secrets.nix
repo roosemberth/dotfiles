191d3411ce6e2336cea6efa278cf4ca9bd96fcf6
Roosembert Palacios <roosemberth@posteo.ch>
2022-12-25 04:58:45 +0100

    secrets: Remove keyring support
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/secrets.nix
+++ nix/secrets.nix
@@ -3,16 +3,7 @@ with lib; assert (assertMsg _modinjector
                   "The secrets module should not be called directly.");
 let
   admins = import ./secrets/users/admins.nix { inherit lib; };
-  keyring-names =
-  let files = builtins.readDir ./secrets/keyrings;
-      keys = attrNames (filterAttrs (name: _: hasSuffix ".asc" name) files);
-  in map (removeSuffix ".asc") keys;
 in recursiveUpdate (rec {
   adminPubKeys = admins.authorizedPublicKeys;
   users = import ./secrets/users/users.nix { inherit lib; };
-  keyrings = genAttrs keyring-names (name: {
-    inherit name;
-    key = ./. + "/secrets/keyrings/${name}.asc";
-    archive = ./. + "/secrets/keyrings/${name}.zip";
-  });
 }) {}
