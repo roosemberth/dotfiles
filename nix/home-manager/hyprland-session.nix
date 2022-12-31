8754b2d818530f1d80586b3032dd28bd60c53431
Roosembert Palacios <roosemberth@posteo.ch>
2022-12-31 02:44:04 +0100

    hyprland-session: Add support for waybar
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/home-manager/hyprland-session.nix
+++ nix/home-manager/hyprland-session.nix
@@ -1,8 +1,34 @@
-{ config, lib, pkgs,  ... }: with lib; {
+{ config, lib, pkgs, dotfileUtils, ... }: with lib; let
+  waybar' = with pkgs; let
+    cfgFile = dotfileUtils.fetchDotfile "etc/waybar/config";
+    styleFile = dotfileUtils.fetchDotfile "etc/waybar/style.css";
+  in stdenv.mkDerivation {
+    name = "waybar-hyprland-with-config";
+    version = waybar-hyprland.version;
+    nativeBuildInputs = [ makeWrapper ];
+
+    buildCommand = ''
+      makeWrapper ${waybar-hyprland}/bin/waybar "$out/bin/waybar" \
+        --prefix PATH : "${lib.makeBinPath [ hyprland pavucontrol procps ]}" \
+        --add-flags "--config ${cfgFile} --style ${styleFile}"
+    '';
+  };
+
+in {
   options.sessions.hyprland.enable = mkEnableOption "Hyprland wayland session";
 
   config = mkIf config.sessions.hyprland.enable {
     session.wayland.enable = true;
+    systemd.user.services.waybar-hyprland = {
+      Unit.Description = "A wayland taskbar for hyprland";
+      Unit.PartOf = [ "hyprland-session.target" ];
+      Install.WantedBy = [ "hyprland-session.target" ];
+      Service = {
+        ExecStart = "${waybar'}/bin/waybar";
+        Restart = "always";
+        RestartSec = "3";
+      };
+    };
 
     wayland.windowManager.hyprland = let
       terminal = "${pkgs.foot}/bin/foot";
