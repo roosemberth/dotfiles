a736841dbdeedd0f20db9edf423537bd23d11998
Roosembert Palacios <roosemberth@posteo.ch>
2023-02-27 12:25:33 +0100

    Fix some partially broken behaviour on nginx-fileshare module
    I'm still not satisfied by this since the module depends on
    implementation details of my network and how I configure them,
    but at least it works for now.
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/nginx-fileshare.nix
+++ nix/modules/nginx-fileshare.nix
@@ -11,21 +11,28 @@ in {
     host = mkOption {
       type = types.str;
       description = "Virtual host serving files";
-      default = "${config.networking.hostName}.orbstheorem.ch";
+      default = "files.rec.la";
     };
   };
 
   config = mkIf config.roos.nginx-fileshare.enable {
     services.nginx.enable = true;
     services.nginx.virtualHosts."${cfg.host}" = {
+      onlySSL = true;
+      sslCertificate = "${pkgs.recla-certs}/rec.la-bundle.crt";
+      sslCertificateKey = "${pkgs.recla-certs}/rec.la-key.pem";
       root = cfg.directory;
       locations."/".extraConfig = "return 307 /public/;";
-      locations."^~ /public".extraConfig = "autoindex on;";
-      locations."~ ^/(.+?)/(.*)$".extraConfig = ''
+      locations."/public".extraConfig = "autoindex on;";
+      locations."~ ^/(?!public)(.+?)/(.*)$".extraConfig = ''
         alias ${cfg.directory}/usr/$1/$2;
         autoindex on;
         auth_basic "Speak friend and come in";
         auth_basic_user_file /srv/shared/usr/$1/.htpasswd;
+      '';
+      extraConfig = ''
+        # NGINX does not have enough information to provide user with an absolute URL.
+        absolute_redirect off;
       '';
     };
   };
