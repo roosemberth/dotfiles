76a97804084eb138a5addd29ac8478dfd030b7f8
Roosembert Palacios <roosemberth@posteo.ch>
2025-02-08 11:31:21 +0100

    Remove timewarrior
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/modules/agenda.nix
+++ nix/modules/agenda.nix
@@ -15,30 +15,8 @@ in {
       home.sessionVariables = rec {
         PASSWORD_STORE_DIR = "${userCfg.xdg.dataHome}/pass";
         GNUPGHOME = "${userCfg.xdg.dataHome}/gnupg";
-        TASKRC = "${userCfg.xdg.configHome}/task/taskrc";
-        TASKDATA = "${userCfg.xdg.dataHome}/task";
         TIMEWARRIORDB = "${userCfg.xdg.dataHome}/timewarrior";
       };
-
-      programs.taskwarrior.enable = true;
-      programs.taskwarrior.config = {
-        dateformat = "Y-M-DTH:N";
-        default.command = "list";
-
-        context.gnu = "project:GNUGen";
-        context.agep = "project:AGEPoly";
-        context.bity = "project:Bity";
-        context.epfl = "project:EPFL";
-
-        color.tag.nohl = "blue";
-
-        report.list.columns = "id,project,tags,priority,start.active,description,due,due.remaining";
-        report.list.labels = "ID,Proj,Tags,Pri,A,Description,Due,";
-      };
-
-      # Should make a module someday...
-      xdg.dataFile."task/hooks/on-modify.timewarrior".source =
-        util.fetchDotfile "etc/task/on-modify.timewarrior";
     };
 
     roos.gConfig = {
