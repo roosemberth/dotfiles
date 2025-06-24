73288723d27f9a92a343ba6b9c3e8c74d86b0cea
Roosembert Palacios <roosemberth@posteo.ch>
2025-06-24 21:14:20 +0200

    Enable fwupd in Janus
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- nix/machines/Janus-static.nix
+++ nix/machines/Janus-static.nix
@@ -85,6 +85,7 @@ in {
     };
   };
 
+  services.fwupd.enable = true;
   services.hardware.bolt.enable = true;
   systemd.services.fix-generated-mounts-permissions = {
     description = "Fix directory permissions of the @roosemberth dataset"
