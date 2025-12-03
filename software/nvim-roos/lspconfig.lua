c680905f7ee9966e5755863b7ff57220979f4e06
Roosembert Palacios <roosemberth@posteo.ch>
2025-12-03 10:23:38 +0100

    nvim-roos: Update config using lspconfig "framework" to nvim API
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- software/nvim-roos/lspconfig.lua
+++ software/nvim-roos/lspconfig.lua
@@ -1,8 +1,6 @@
 vim.o.inccommand = 'nosplit'
 vim.o.completeopt = 'menuone,noselect'
 
-local lspconfig = require'lspconfig'
-
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
@@ -16,41 +14,44 @@ vim.keymap.set('n', '<leader>adw', function()
   vim.diagnostic.setqflist({severity = "W"})
 end, opts)
 
-local on_generic_lsp_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'I', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>aa', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>ar', vim.lsp.codelens.run, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', function()
-    vim.lsp.buf.format { async = true }
-  end, bufopts)
-end
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'I', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>aa', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>ar', vim.lsp.codelens.run, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, bufopts)
+  end
+})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "rust",
   callback = function(ev)
-    on_generic_lsp_attach(null, ev.buf)
     local bufopts = { noremap=true, silent=true, buffer=ev.buf }
     vim.keymap.set('n', '<leader>aa', function()
       vim.cmd.RustLsp('codeAction')
     end, bufopts)
-    vim.keymap.set('n', 'I', function()
+    vim.keymap.set('n', 'K', function()
       vim.cmd.RustLsp({ 'hover', 'actions' })
     end, bufopts)
     vim.keymap.set('n', '<leader>rt', function()
@@ -59,30 +60,31 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-lspconfig.hls.setup {
-  on_attach = on_generic_lsp_attach,
+vim.lsp.config('hls', {
   settings = {
     haskell = {
       formattingProvider = "fourmolu",
     }
   }
-}
+})
 
-lspconfig.pylsp.setup {
-  on_attach = on_generic_lsp_attach,
-}
+vim.lsp.config('pylsp', {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 80
+        }
+      }
+    }
+  }
+})
 
-lspconfig.clangd.setup {
-  on_attach = on_generic_lsp_attach,
-}
-
-vim.g.markdown_fenced_languages = {
-  "ts=typescript"
-}
-
-lspconfig.tsserver.setup {
-  on_attach = on_generic_lsp_attach,
-}
+vim.lsp.enable('clangd')
+vim.lsp.enable('hls')
+vim.lsp.enable('pylsp')
+vim.lsp.enable('ts_ls')
 
 -- LSP Status and progress
 require("fidget").setup {
